593ce80de1e21487ce7c738c4fcdcdc435894220
Maurice van Creij <maurice.vancreij@webqem.com>
2019-02-22 07:55:42 +1100

    Update require support.

--- src/js/typeout.js
+++ src/js/typeout.js
@@ -34,6 +34,5 @@ var TypeOut = function (config) {
 };
 
 // return as a require.js module
-if (typeof module !== 'undefined') {
-	exports = module.exports = TypeOut;
-}
+if (typeof define != 'undefined') define(['typeout'], function () { return TypeOut });
+if (typeof module != 'undefined') module.exports = TypeOut;
